5da5ce1ed53afd51258cbee2c7669c314ab36cbb
windsonleo <windsor_melo@hotmail.com>
2017-11-06 05:28:07 -0300

    Query referindo se ao nome do atributo

--- src/main/java/com/tecsoluction/restaurante/dao/IPedidoVendaDAO.java
+++ src/main/java/com/tecsoluction/restaurante/dao/IPedidoVendaDAO.java
@@ -11,7 +11,7 @@ import com.tecsoluction.restaurante.entidade.PedidoVenda;
 public interface IPedidoVendaDAO extends org.springframework.data.jpa.repository.JpaRepository<PedidoVenda, String> {
 
 
-	@Query("SELECT p FROM PedidoVenda p where p.mesa_id=:idmesa")
+	@Query("SELECT p FROM PedidoVenda p where p.mesa=:idmesa")
 	public List<PedidoVenda> getAllPedidoPorMesa(@Param("idmesa")String idmesa);
 
 	@Query("SELECT p FROM PedidoVenda p where p.data=:dataini")
