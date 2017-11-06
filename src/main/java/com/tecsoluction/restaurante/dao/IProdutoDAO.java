f27b68a5dc5cdfb4b44b842734731d23fa48ea05
windsonleo <windsor_melo@hotmail.com>
2017-11-06 05:12:50 -0300

    Ajuste de IProdutoDAO

--- src/main/java/com/tecsoluction/restaurante/dao/IProdutoDAO.java
+++ src/main/java/com/tecsoluction/restaurante/dao/IProdutoDAO.java
@@ -10,10 +10,10 @@ import com.tecsoluction.restaurante.entidade.Produto;
 @org.springframework.stereotype.Repository
 public interface IProdutoDAO extends org.springframework.data.jpa.repository.JpaRepository<Produto, String> {
 	
-	@Query("SELECT p FROM Produto p where p.codebar=(:codebar)")
+	@Query("SELECT p FROM Produto p where p.codebar=:codebar")
 	public Produto getProdutoPorCodebar(@Param("codebar") String codebar) ;
 	
-	@Query("SELECT p FROM Produto p where p.categoria=")
+	@Query("SELECT p FROM Produto p where p.categoria=:idcategoria")
 	public List<Produto> getAllProdutoPorCategoria(@Param("idcategoria") String idcategoria); 
 
 	
