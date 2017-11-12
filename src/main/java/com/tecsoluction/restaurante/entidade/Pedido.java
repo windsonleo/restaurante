9d5e91877bb90675957234066c1f6b4d49248d5b
cleberson <clebrsonn@hotmail.com>
2017-11-12 09:05:31 -0300

    update pom.xml

--- src/main/java/com/tecsoluction/restaurante/entidade/Pedido.java
+++ src/main/java/com/tecsoluction/restaurante/entidade/Pedido.java
@@ -29,7 +29,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Pedido extends BaseEntity {
